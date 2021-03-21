499be1b9e186c288d2fa733b12d6bea1cf9fe63e
Clarinha Prado <clarinha.prado@gmail.com>
2021-03-21 20:26:29 -0300

    finalização da aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logoImg from '../../assets/logo.svg';
 import { Button, Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <Button type="button">
+                <Button
+                    type="button"
+                    onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </Button>
             </Content>
